2e3f468dc13a0891a52f877a4a98209f14af4081
literate <gaekoon@gmail.com>
2023-12-20 15:55:21 +0900

    Feat: 불필요 코드 제거

--- src/main/java/io/security/corespringsecurity/controller/user/UserController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -1,21 +1,11 @@
 package io.security.corespringsecurity.controller.user;
 
 
-import io.security.corespringsecurity.domain.Account;
-import io.security.corespringsecurity.domain.AccountDto;
-import io.security.corespringsecurity.service.UserService;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-
-	@Autowired
-	private UserService userService;
 
 	@GetMapping(value="/mypage")
 	public String myPage() throws Exception {
