40e4d9613e9a68b876e2af998e883d8e0b27bdee
literate <gaekoon@gmail.com>
2023-12-20 15:33:33 +0900

    Feat: 정적 자원에 WebIgnore 설정

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -14,11 +16,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
+  public void configure(WebSecurity web) throws Exception {
+    // permitAll()과의 차이점
+    // 이 친구는 보안 필터를 거치지 않으니 비용이 저렴함
+    // 확인은 FilterSecurityInterceptor 클래스에서
+    // invoke()의 InterceptorStatusToken token = super.beforeInvocation(fi)에서 확인 가능
+    // 이 설정을 안 끄면 static 파일들의 경로로 fi 객체에서 확인할 수 있음
+    web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
+
+  @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     String password = passwordEncoder().encode("1111");
     auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-    auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-    auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+    auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+    auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER");
   }
 
   @Bean
